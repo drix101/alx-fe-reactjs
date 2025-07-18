591b943c91556728b45a4d421fca1ce4944dac4c
Hendrix <hoyheenlawlarlawal@gmail.com>
2025-07-18 23:42:52 +0100

    Create your first React App

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,6 +3,11 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import UserProfile from './components/UserProfile'
+import Footer from './components/Footer';
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +16,10 @@ function App() {
     <>
       <div>
         <WelcomeMessage /> 
+        <Header />
+        <MainContent />
+        <UserProfile name="Alice" age="25" bio="Loves hiking and photography" /> 
+        <Footer />
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
