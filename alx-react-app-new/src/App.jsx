706ea46830174b56611b72b00ba645593ae8233d
Hendrix <hoyheenlawlarlawal@gmail.com>
2025-07-18 23:54:06 +0100

    Styling, States, Rendering, Routes and Hooks

---
+++ alx-react-app-new/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import UserProfile from './components/UserProfile'
+import Footer from './components/Footer';
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <WelcomeMessage /> 
+        <Header />
+        <MainContent />
+        <UserProfile name="Alice" age="25" bio="Loves hiking and photography" /> 
+        <Footer />
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+    </>
+  )
+}
+
+export default App
