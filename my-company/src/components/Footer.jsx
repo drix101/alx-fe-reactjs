776a2b969e75c65b03da3a17924dd8e14707b3d8
Hendrix <hoyheenlawlarlawal@gmail.com>
2025-07-20 22:31:17 +0100

    Build a Simple Company Website with React

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer style={{ textAlign: "center", padding: "1rem", marginTop: "2rem"}}>
+      <p>&copy; 2025 My Company. All rights reserved.</p>
+    </footer>
+  );
+}
+export default Footer;
