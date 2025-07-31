a068cf5ee5d75095c5c4751e7da411c9da577df2
Hendrix <hoyheenlawlarlawal@gmail.com>
2025-07-31 15:21:36 +0100

    Integrating the GitHub API and Creating a Basic Search Feature

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import Search from './components/Search';
 
 function App() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 p-8">
       <h1 className="text-3xl font-bold mb-4">GitHub User Search App</h1>
       {/* Future components like <Search /> will be added here */}
-      <Search />  
     </div>
   );
 }
--- github-user-search/src/services/githubService.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-export const fetchUserData = async (username) => {
-  const url = `https://api.github.com/users/${username}`;
-  const response = await axios.get(url);
-  return response.data;
-};
