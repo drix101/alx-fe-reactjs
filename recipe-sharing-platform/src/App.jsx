859ce18e382925f898b3b06f7e946c1407841686
Hendrix <hoyheenlawlarlawal@gmail.com>
2025-08-12 13:57:18 +0100

    Building the Home Page of the Recipe Sharing Platform

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -2,11 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Homepage from './components/Homepage'
 
 function App() {
   return (
     <h1 className="text-3xl font-bold text-blue-500">
-      Recipe Sharing Platform Setup Complete 🎉
+     <Homepage />
     </h1>
   );
 }
--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,4 +1,5 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
+// This is a Tailwind CSS configuration file for a recipe sharing platform.
 module.exports = {
   content: ['./public/index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
